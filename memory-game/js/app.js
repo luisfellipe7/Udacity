8f0385c079bec8805f9bdeef272b6d61223dcc65
Luis Fellipe Furio Raphael <lfraphael@gmail.com>
2018-02-27 14:45:44 -0300

    Add files via upload

---
+++ memory-game/js/app.js
@@ -0,0 +1,195 @@
+/*
+ * Create a list of the cards
+ */
+var listCards = ["fa-chess-pawn", "fa-chess-knight", "fa-chess-king", "fa-chess-queen", "fa-chess-bishop", "fa-chess", "fa-chess-rook", "fa-bomb"];
+// to store number of moves and matches found
+var moves = 0;
+var found_card = 0;
+
+// check when first card is opened
+var start_game = false;
+
+// timer object
+var timer = new Timer();
+timer.addEventListener('secondsUpdated', function (e) {                   $('#timer').html(timer.getTimeValues().toString());
+});
+
+// reference to restart button
+$('#reset-button').click(resetGame);
+// create and append card html
+function createCard(card) {
+    $('#deck').append(`<li class="card animated"><i class="fa ${card}"></i></li>`);
+}
+// generate random cards on the board
+function generateCards() {
+    for (var i = 0; i < 2; i++) {
+        listCards = shuffle(listCards);
+        listCards.forEach(createCard);
+    }
+}
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = array.length
+        , temporaryValue, randomIndex;
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
+}
+// Array to keep track of the cards
+openCards = [];
+
+// card functionality
+function toggleCard() {
+    
+    // start the timer when first card is opened
+    if (start_game == false) {
+        start_game = true;
+        timer.start();
+    }
+    
+    if (openCards.length === 0) {
+        $(this).toggleClass("show open").animateCss('flipInY');
+        openCards.push($(this));
+        disableCLick();
+    }
+    else if (openCards.length === 1) {
+        // increment moves
+        updateMoves();
+        $(this).toggleClass("show open").animateCss('flipInY');
+        openCards.push($(this));
+        setTimeout(matchOpenCards, 1100);
+    }
+}
+// Disable click of the open Cards
+function disableCLick() {
+    openCards.forEach(function (card) {
+        card.off('click');
+    });
+}
+// enable click on the open card
+function enableClick() {
+    openCards[0].click(toggleCard);
+}
+// check openCards if they match or not
+function matchOpenCards() {
+    if (openCards[0][0].firstChild.className == openCards[1][0].firstChild.className) {
+        console.log("matchCard");
+        openCards[0].addClass("match").animateCss('pulse');
+        openCards[1].addClass("match").animateCss('pulse');
+        disableCLick();
+        removeOpenCards();
+        setTimeout(checkWin, 1000);
+    }
+    else {
+        openCards[0].toggleClass("show open").animateCss('flipInY');
+        openCards[1].toggleClass("show open").animateCss('flipInY');
+        enableClick();
+        removeOpenCards();
+    }
+}
+// function to remove openCards
+function removeOpenCards() {
+    openCards = [];
+}
+// function to add animations
+$.fn.extend({
+    animateCss: function (animationName) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        this.addClass(animationName).one(animationEnd, function () {
+            $(this).removeClass(animationName);
+        });
+        return this;
+    }
+});
+// update moves
+function updateMoves() {
+    moves += 1;
+    $('#moves').html(`${moves} Moves`);
+    if (moves == 24) {
+        addBlankStar();
+    }
+    else if (moves == 15) {
+        addBlankStar();
+    }
+}
+// check whether the game is finished or not 
+function checkWin() {
+    found_card += 1;
+    if (found_card == 8) {
+        showResults();
+    }
+}
+// add blank stars
+function addBlankStar() {
+    $('#stars').children()[0].remove();
+    $('#stars').append('<li><i class="fa fa-star-o"></i></li>');
+}
+// add initial stars
+function addStars() {
+    for (var i = 0; i < 3; i++) {
+        $('#stars').append('<li><i class="fa fa-star"></i></li>');
+    }
+}
+// reset the game
+function resetGame() {
+    moves = 0;
+    found_card = 0;
+    $('#deck').empty();
+    $('#stars').empty();
+    $('#game-deck')[0].style.display = "";
+    $('#sucess-result')[0].style.display = "none";
+    start_game=false;
+    timer.stop();
+    $('#timer').html("00:00:00");
+    playGame();
+}
+// Init function
+function playGame() {
+    generateCards();
+    $('.card').click(toggleCard);
+    $('#moves').html("0 Moves");
+    addStars(3);
+}
+// shows result on end game
+function showResults() {
+    $('#sucess-result').empty();
+    timer.pause();
+    var scoreBoard = `
+        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 130.2 130.2">
+            <circle class="path circle" fill="none" stroke="#73AF55" stroke-width="6" stroke-miterlimit="10" cx="65.1" cy="65.1" r="62.1" />
+            <polyline class="path check" fill="none" stroke="#73AF55" stroke-width="6" stroke-linecap="round" stroke-miterlimit="10" points="100.2,40.2 51.5,88.8 29.8,67.5 " /> </svg>
+        <p class="success"> Congrats !!! </p>
+        <p>
+            <span class="score-titles">Moves:</span>
+            <span class="score-values">${moves}</span>
+            <span class="score-titles">Time:</span>
+            <span class="score-values">${timer.getTimeValues().toString()}</span>
+        </p>
+        <div class="text-center margin-top-2">
+             <div class="star">
+                <i class="fa fa-star fa-3x"></i>    
+             </div>
+             <div class="star">
+                <i class="fa ${ (moves > 23) ? "fa-star-o" : "fa-star"}  fa-3x"></i>    
+             </div>
+            <div class="star">
+                <i class="fa ${ (moves > 14) ? "fa-star-o" : "fa-star"} fa-3x"></i>    
+             </div>
+        </div>
+        <div class="text-center margin-top-2" id="restart">
+            <i class="fa fa-repeat fa-2x"></i>
+          </div>
+    `;
+    $('#game-deck')[0].style.display = "none";
+    $('#sucess-result')[0].style.display = "block";
+    $('#sucess-result').append($(scoreBoard));
+    $('#restart').click(resetGame);
+}
+
+// start the game
+playGame();
