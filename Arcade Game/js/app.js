a2a69a582deb90cb4eaed18091f0df1154da1aa0
Luis Fellipe Furio Raphael <lfraphael@gmail.com>
2018-05-14 12:30:42 -0300

    Add files via upload

--- Arcade Game/js/app.js
+++ Arcade Game/js/app.js
@@ -1,125 +1,157 @@
 // Enemies our player must avoid
-var Enemy = function (x, y, speed) {
-
-    // The following variables are used to determine the x and y axis and speed of the enemy
+var Enemy = function(x, y, speed) {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
     this.x = x;
     this.y = y;
     this.speed = speed;
 
-    // The image of the enemy of cockroach that is added to the playing field 
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function (dt) {
-
-    // Multiplies the speed by the dt parameter on the x axis
-    this.x += this.speed * dt;
-
-    // Once enemies are off the canvas, they reappear randomly with different speeds
-    if (this.x > 510) {
-        this.x = -50;
-        this.speed = 100 + Math.floor(Math.random() * 222);
-    };
-
-    // Checks for collisions between the player and the enemies
-    if (player.x < this.x + 80 &&
-        player.x + 80 > this.x &&
-        player.y < this.y + 60 &&
-        60 + player.y > this.y) {
-        player.x = 202;
-        player.y = 405;
-    };
+Enemy.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    this.x = this.x + this.speed * dt;
+    if (this.x >= 505) {
+        this.x = 0;
+    }
+    this.checkCollision();
 };
 
-// Renders the enemy into the game
-Enemy.prototype.render = function () {
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Player class focusing on x and y axis
-var Player = function (x, y) {
+// checks en enemy's collision with player
+Enemy.prototype.checkCollision = function() {
+    if (player.y + 131 >= this.y + 90 &&
+        player.y + 73 <= this.y + 135 &&
+        player.x + 25 <= this.x + 88 &&
+        player.x + 76 >= this.x + 11) {
+        gameReset();
+    }
+};
 
-    // Variables for the player to move along x and y axis 
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+var Player = function(x, y, speed) {
     this.x = x;
     this.y = y;
-
-    //The image of the player of horn-girl is added to the playing field 
-    this.player = 'images/char-horn-girl.png';
+    this.speed = speed;
+    this.sprite = 'images/char-boy.png';
 };
 
-Player.prototype.update = function (dt) {
+// Update method for Player
+Player.prototype.update = function() {};
 
+// renders the player
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Renders the image of the user into the game
-Player.prototype.render = function () {
-    ctx.drawImage(Resources.get(this.player), this.x, this.y);
+/*
+ * handles input for the player
+ */
+Player.prototype.handleInput = function(key) {
+    if (key == 'left') {
+        this.x = (this.x - this.speed + 505) % 505;
+    } else if (key == 'right') {
+        this.x = (this.x + this.speed) % 505;
+    } else if (key == 'up') {
+        this.y = (this.y - this.speed + 606) % 606;
+        // going to water
+        if (this.y <= (83 - 48)) { // line 135 engine.js
+            // assuming 48 to be player height
+            gameOver();
+            return;
+        }
+    } else {
+        this.y = (this.y + this.speed) % 606;
+        if (this.y > 400) { // bottom limit
+            this.y = 400;
+        }
+    }
+    // x axis wrap
+    if (this.x < 2.5) {
+        this.x = 2.5;
+    }
+    if (this.x > 458) {
+        this.x = 458;
+    }
 };
 
-// Allows the user to use the arrow keys to jump from tile to tile
-Player.prototype.handleInput = function (keyPress) {
-
-    // Enables user on left arrow key to move left on the x axis by 102
-    // Also enables user not to go off the game tiles on the left side
-    if (keyPress == 'left' && this.x > 0) {
-        this.x -= 102;
-    };
-
-    // Enables user on right arrow key to move right on the x axis by 102
-    // Also enables user not to go off the game tiles on the right side
-    if (keyPress == 'right' && this.x < 405) {
-        this.x += 102;
-    };
-
-    // Enables user on up arrow key to move upwards on the y axis by 83
-    if (keyPress == 'up' && this.y > 0) {
-        this.y -= 83;
-    };
-
-    // Enables user on down arrow key to move downwards on the y axis by 83
-    // Also enables user not to go off the game tiles on the bottom side
-    if (keyPress == 'down' && this.y < 405) {
-        this.y += 83;
-    };
-
-    // Once the user reaches the top of the page; the water, the user is
-    // Instantly reset to the starting position
-    if (this.y < 0) {
-        setTimeout(() => {
-            this.x = 202;
-            this.y = 405;
-        }, 800);
-    };
+/*
+ * resets the player to default position
+ */
+Player.prototype.reset = function() {
+    this.x = 202.5;
+    this.y = 383;
 };
 
-
-// All enemies are placed in an array
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
 var allEnemies = [];
-
-// Location of the 3 enemies on the y axis located on the stone road
-var enemyLocation = [63, 147, 230];
-
-
-// For each enemy located on the y axis from 0 on the x axis move at a speed of 200 
-// Until randomly regenerated in the enemy update function above
-enemyLocation.forEach(function (locationY) {
-    enemy = new Enemy(0, locationY, 200);
-    allEnemies.push(enemy);
-});
-
-// The starting location of the player is located at x=200, y=405
-var player = new Player(202, 405);
+var player = new Player(0, 0, 50);
+var scoreDiv = document.createElement('div');
+gameReset(); // setup defaults
+var canvasDiv = document.getElementsByTagName('canvas')[0];
+document.body.insertBefore(scoreDiv, canvasDiv);
 
 // This listens for key presses and sends the keys to your
-// Player.handleInput() method. 
-document.addEventListener('keyup', function (e) {
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
         40: 'down'
     };
+
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+// custom
+// increase enemies by score
+var score = 0;
+
+/*
+ * resets the game in case of collision
+ */
+function gameReset() {
+    player.reset();
+    score = 0;
+    updateDisplay();
+    allEnemies = [];
+    allEnemies.push(
+        new Enemy(0, Math.random() * 150 + 50, Math.random() * 100 + 40),
+        new Enemy(0, Math.random() * 150 + 70, Math.random() * 100 + 60)
+    );
+}
+
+/*
+ * game over successfully (reached water)
+ */
+function gameOver() {
+    player.reset();
+    score += 1;
+    updateDisplay();
+    if (score % 2 == 0 && allEnemies.length < 4) {
+        allEnemies.push(new Enemy(0, Math.random() * 160 + 50, Math.random() * 90 + 70));
+    }
+}
+
+/*
+ * updates the on screen score display
+ */
+function updateDisplay() {
+    scoreDiv.innerHTML = 'Score ' + score;
+}
